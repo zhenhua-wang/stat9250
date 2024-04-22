1b3f7a68c85c1a2ce752e8cb6c372fb6b7fe3414
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-04-22 13:33:27 -0500

    same as previous

--- final/gradient_methods.r
+++ final/gradient_methods.r
@@ -144,23 +144,6 @@ bootstrap_predict <- function(Y, X, result_boot) {
 }
 
 ## * analysis using Newton-Raphson
-## load data
-Y <- heart$HeartDisease
-X <- heart %>% dplyr::select(-HeartDisease)
-X$Smoking <- as.factor(X$Smoking)
-X$Sex <- as.factor(X$Sex)
-X <- one_hot(as.data.table(X))
-X <- as.matrix(X)
-X <- cbind(rep(1, nrow(X)), X)
-
-## split train test
-shuffled_indices <- sample(nrow(X))
-train_idx <- shuffled_indices[1:round(0.7 * nrow(X))]
-X_train <- X[train_idx, ]
-Y_train <- Y[train_idx]
-X_test <- X[-train_idx, ]
-Y_test <- Y[-train_idx]
-
 ## training
 start.time <- Sys.time()
 ## result <- gradient_descient(Y_train, X_train,
